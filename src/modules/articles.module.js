5aaf42bcb2fcae2f902a95ba0b3b8dc3dfb40524
marwa <alqtta04@gmail.com>
2023-11-05 22:09:47 +0200

    add my schema

---
+++ src/modules/articles.module.js
@@ -0,0 +1,44 @@
+const validator = require('validator')
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const articleSchema = new Schema({
+title:{
+    type:String,
+    required : true , 
+    trim:true
+}
+,
+content:{
+    type:String,
+    required : true , 
+    
+},image:{
+    type:String,
+    required : true , 
+    trim:true
+    
+},
+category:{
+    type:String,
+    required : true , 
+    
+},
+    date: {
+        type:String ,
+        required : true , 
+    trim:true
+    },
+    status:{
+        type:String,
+    required : true , 
+    },
+    owner:{
+      type: mongoose.Types.ObjectId ,
+      ref:"user" ,
+      required:true
+    }
+
+})
+const Article =mongoose.model("article",articleSchema);
+module.exports= Article
--- src/routes/articales.routes.js
+++ src/routes/articales.routes.js
@@ -1,1 +1,3 @@
-
+const router = require('express').Router();
+router.route("/article").get().post()
+router.route("/article/:id").get().post()
