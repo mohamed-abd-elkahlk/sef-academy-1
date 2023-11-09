0846d8663e946e7ca4fc9cdf71912f55a573d62f
mohamed abd elkhalk <01147018091.m@gmial.com>
2023-11-09 20:55:12 +0200

    mounting the routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { logout } = require("../services/auth.service");
+
 router.post("/login", loginValidator, login);
 
 router.post("/logout", logout);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 
 const categoryRoute = require("./category.routes");
+const authRoute = require("./auth.routes");
+const articalesRoute = require("./articales.routes");
+const jopsRoute = require("./jops.routes");
+const userRoute = require("./user.routes");
+const coursesRoute = require("./courses.routes");
 
 router.use("/category", categoryRoute);
+router.use("/auth", authRoute);
+router.use("/articales", articalesRoute);
+router.use("/jop", jopsRoute);
+router.use("/user", userRoute);
+router.use("/courses", coursesRoute);
 
 module.exports = router;
