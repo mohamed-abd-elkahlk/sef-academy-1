2bbcb6a40bce4f8d436501bc16525a897b5c2037
mohamed abd elkhalk <01147018091.m@gmial.com>
2023-11-10 19:50:18 +0200

    bugs

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const { ApiError } = require("../utils");
- const {verifyPasswordHash} = require("../utils");
- const {issueJwt} = require("../utils");
- const {genPasswordHash} = require("../utils");
+const { verifyPasswordHash, issueJwt } = require("../utils");
 const User = require("../modules/user.module");
 
 exports.allowedTo = (...roles) =>
@@ -15,24 +13,26 @@ exports.allowedTo = (...roles) =>
     next();
   });
 
-exports.logout = asyncHandler(async (req, res, next) => {});
-const Login = asyncHandler(async (req, res, next) =>{
+exports.login = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ userId: req.body.userId });
-  if(!user) { 
-    return 
-      next( new ApiError("Invalid userId or password", 401) );
-    }
-    const verifyPassword = verifyPasswordHash(
-  req.body.password,
-      user.salt,
-      user.password
-    );
-      if (!verifyPassword) {
-      return next(new ApiError("Incorrect userId or password", 401)); }
-   const token = issueJwt(user);
-  res.status(200)
-  .cookie("jwt", token,
-   { sameSite: "Strict", httpOnly: true }).json({ data: user })
-   //user.tokens.push(token )
-//await user.save()
-   });
+  if (!user) {
+    return;
+    next(new ApiError("Invalid userId or password", 401));
+  }
+  const verifyPassword = verifyPasswordHash(
+    req.body.password,
+    user.salt,
+    user.password
+  );
+  if (!verifyPassword) {
+    return next(new ApiError("Incorrect userId or password", 401));
+  }
+  const token = issueJwt(user);
+  res
+    .status(200)
+    .cookie("jwt", token, { sameSite: "Strict", httpOnly: true })
+    .json({ data: user });
+  //user.tokens.push(token )
+  //await user.save()
+});
+exports.logout = asyncHandler(async (req, res, next) => {});
