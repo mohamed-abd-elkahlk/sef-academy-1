0c456fcf745477b3aed1c54fb3ea50db5f6265f1
marwa <alqtta04@gmail.com>
2023-11-10 17:13:03 +0200

    update my auth_task

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const { ApiError } = require("../utils");
+ const {verifyPasswordHash} = require("../utils");
+ const {issueJwt} = require("../utils");
+ const {genPasswordHash} = require("../utils");
 const User = require("../modules/user.module");
 
 exports.allowedTo = (...roles) =>
@@ -13,3 +16,23 @@ exports.allowedTo = (...roles) =>
   });
 
 exports.logout = asyncHandler(async (req, res, next) => {});
+const Login = asyncHandler(async (req, res, next) =>{
+  const user = await User.findOne({ userId: req.body.userId });
+  if(!user) { 
+    return 
+      next( new ApiError("Invalid userId or password", 401) );
+    }
+    const verifyPassword = verifyPasswordHash(
+  req.body.password,
+      user.salt,
+      user.password
+    );
+      if (!verifyPassword) {
+      return next(new ApiError("Incorrect userId or password", 401)); }
+   const token = issueJwt(user);
+  res.status(200)
+  .cookie("jwt", token,
+   { sameSite: "Strict", httpOnly: true }).json({ data: user })
+   //user.tokens.push(token )
+//await user.save()
+   });
