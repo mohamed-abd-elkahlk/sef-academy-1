4c356be85a4cef0654236800b133a887b5faba45
MohmedAref31 <mohmedwork2003@gmail.com>
2023-11-11 06:45:49 +0200

    create exam model and certificate model

---
+++ src/modules/certificate.module.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+
+
+const certificateSchema = new mongoose.Schema({
+    student:{
+        type:mongoose.Types.ObjectId,
+        ref:"User",
+        required:[true,"student id is required"]
+    },
+    course:{
+        type:mongoose.Types.ObjectId,
+        ref:"Course",
+        required:[true,"course id is required"]
+    },
+    duration:{
+        type:String,
+        required:[true,"duration is required"]
+    }
+},{timestamps:true});
+
+
+
+
+exports.Certificate = mongoose.model("Certificate", certificateSchema)
