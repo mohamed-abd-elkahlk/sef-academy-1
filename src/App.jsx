11bb4005b9ffab1eb654dbe2eb7eef13e64c14b4
mohamed abd elkhalk <01147018091.m@gmial.com>
2023-11-18 23:30:16 +0200

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "./layout/Footer";
+import LoginForm from "./components/amany/LoginForm";
 function App() {
   return (
     <Container>
-      <Footer />
+      <LoginForm />
     </Container>
   );
 }
