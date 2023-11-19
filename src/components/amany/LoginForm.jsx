a983ff1da2527910f584f82701afed798c877478
Alsakr922 <mo7amedk7aled922.com>
2023-11-19 17:16:19 +0200

    A login form style by mohamed

--- src/components/amany/LoginForm.jsx
+++ src/components/amany/LoginForm.jsx
@@ -11,11 +11,12 @@ import {
   MDBCheckbox,
 } from "mdb-react-ui-kit";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./amany.css";
 import { PiUserRectangle } from "react-icons/pi";
 import { BiShow } from "react-icons/bi";
 import Logo from "../../assets/Images/seff_logo_black.jpg";
+import "./log.css";
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({
@@ -35,20 +36,20 @@ const LoginForm = () => {
   };
 
   return (
-    <div className="login-form-container container-overlay">
-      <div className="logo-container">
-        <img src={Logo} alt="Logo" className="logo-img" />
+    <div className="login-form-container container-overlay  ">
+      <div className="logo-container position-relative">
+        <img src={Logo} alt="Logo" className="logo-img img-fluid rounded-circle position-absolute" />
       </div>
       <MDBContainer fluid>
         <MDBRow className=" d-flex justify-content-center align-items-center h-100">
-          <MDBCol col="12">
-            <MDBCard className=" my-5 mx-auto">
+          <MDBCol col="12 py-5 ">
+            <MDBCard className="pt-5 my-5 mx-auto logform rounded-fill">
               <MDBCardBody
-                className="bg1 
-            p-5 w-100 d-flex flex-column"
-              >
+                className="bg1 p-5 w-100 d-flex flex-column">
+                <div className="position-relative">
                 <MDBInput
-                  wrapperClass="mb-4 w-100"
+                  wrapperClass="mb-4 w-100 ps-5 "
+                  className="bg-grey log-form py-3"
                   placeholder="USER ID"
                   id="formControlLg"
                   type="email"
@@ -57,9 +58,13 @@ const LoginForm = () => {
                   value={formData.userId}
                   onChange={handleInputChange}
                 />
+                  <img src={Logo} alt="Logo" className="icon-user" />
+                </div>
+                <div className="position-relative">
                 <MDBInput
-                  wrapperClass="mb-4 w-100"
-                  placeholder="password"
+                  wrapperClass="w-100 ps-5"
+                  className="bg-grey log-form py-3"
+                  placeholder="Password"
                   id="formControlLg"
                   type="password"
                   size="lg"
@@ -67,12 +72,15 @@ const LoginForm = () => {
                   value={formData.password}
                   onChange={handleInputChange}
                 />
+                <img src={Logo} alt="Logo" className="icon-pass" />
+                <img src={Logo} alt="Logo" className="showPass" />
+                </div>
+                <p className="pForm py-3 text-end text-light">Forget your password?</p>
 
-                <p className="pForm">Forget your password?</p>
-
-                <MDBBtn style={{ backgrounColor: "#bf9b30" }}>Login</MDBBtn>
+                <MDBBtn className="btn btn-main border-none py-3 text-light">LOGIN</MDBBtn>
               </MDBCardBody>
             </MDBCard>
+            <p className="">Dont Have an acount yet? <a href="#" className="text-main">sign up</a></p>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
